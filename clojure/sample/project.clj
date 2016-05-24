645addf3ae79cbb92efee537e45cdd3d74421708
Renan Palmeira <renan@forleven.com>
2016-05-24 09:17:00 -0300

    Initial use Clojure with Jetty

--- clojure/sample/project.clj
+++ clojure/sample/project.clj
@@ -1,4 +1,6 @@
 (defproject sample "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :main sample.core)
+	:description "FIXME: write description"
+	:dependencies [[ring "1.4.0"]
+				   [compojure "1.5.0"]
+				   [org.clojure/clojure "1.3.0"]]
+	:main sample.core)
--- clojure/sample/src/sample/core.clj
+++ clojure/sample/src/sample/core.clj
@@ -1,6 +1,11 @@
-(ns sample.core)
+(ns sample.core
+	(:use ring.adapter.jetty))
+
 (defn square [x] (* x x))
-(defn -main
-	"I don't do a whole lot...yet." 
-	[& args]
-	(println "Hello, World!"))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
+
+(run-jetty handler {:port 8080})
