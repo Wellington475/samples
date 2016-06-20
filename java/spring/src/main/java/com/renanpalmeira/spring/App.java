4d385d776841470fb99aa52f8f2ef1f88f57d7e0
Renan Palmeira <renan@forleven.com>
2016-06-19 22:32:40 -0300

    :coffee: Using Spring Framework/Spring Boot in Java. http://goo.gl/DxEjvU

---
+++ java/spring/src/main/java/com/renanpalmeira/spring/App.java
@@ -0,0 +1,25 @@
+package com.renanpalmeira.spring;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Hello world!
+ *
+ */
+@RestController
+@EnableAutoConfiguration
+public class App {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+
+}
