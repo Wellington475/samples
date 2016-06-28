f9357e960ea9fe9f71647c8cedb81448d4e722c0
Renan Palmeira <renan@forleven.com>
2016-06-27 23:59:55 -0300

    :gem: Added methods of API with Sinatra/Response JSON. https://goo.gl/DVjDJl

--- ruby/sinatra/index.rb
+++ ruby/sinatra/index.rb
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+
+require "json"
 require "sinatra"
 require "sinatra/reloader"
+require "sinatra/json"
+
+item = ["clojure", "python", "ruby", "lisp"]
 
 get '/' do
 	'Hello with Sinatra'
@@ -8,4 +13,18 @@ end
 
 get '/:name' do
 	"Hi, #{params['name'].capitalize}"
+end
+
+get '/api/items' do
+	json({
+			:data => item,
+			:count => item.count,
+			:next => nil,
+			:previus => nil,
+		})
+end
+
+get '/api/item/:id' do
+	id = Integer(params[:id])
+	json :name => item[id]
 end
