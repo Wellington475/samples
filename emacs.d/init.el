4c0846fb3ad9ea5ca29618756be6f549a09f9cb2
Renan Palmeira <renan@forleven.com>
2016-08-08 23:24:54 -0300

    :fire: Update Emacs plugins and if's in init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,10 +1,11 @@
 ;; Inibe tela 'inicial', *scratch* buffer será exibido
-(setq inhibit-startup-screen t)
+;; (setq inhibit-startup-screen t)
+(desktop-save-mode 1)
 	
 ;; Remover a barra superior.
 (when (fboundp 'tool-bar-mode)
 (tool-bar-mode -1))
-	
+
 ;; Removendo scroolbars nativas do OS, redundantes
 (when (fboundp 'scroll-bar-mode)
 (scroll-bar-mode -1))
@@ -18,13 +19,27 @@
 ;; Mostrar o path completo para o arquivo na barra superior
 (setq-default frame-title-format "%b (%f)")
 
-;; ;; Packages
+;; Removendo scroolbars nativas do OS, redundantes
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+
+;; Enable linum
+
+(setq linum-mode t)
+
+;; Remove backup files backup~ and #autosave#
+;; http://goo.gl/ojcJjL
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+;; Coding 
+
+(define-coding-system-alias 'utf8 'utf-8)
+
+;; Package
 
 (require 'package)
-;;(require 'rainbow-delimiters)
-(add-hook 'foo-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-
 (package-initialize)
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
@@ -32,9 +47,34 @@
 			 ("melpa" . "http://melpa.org/packages/")))
 
 (unless (package-installed-p 'cider)
-  (package-install 'cider))
+	(package-install 'cider))
+
+(unless (package-installed-p 'rainbow-delimiters)
+	(package-install 'rainbow-delimiters))
 
 (unless (package-installed-p 'monokai-theme)
-  (package-install 'monokai-theme))
+	(package-install 'monokai-theme))
 
-(load-theme 'monokai t)
+(unless (package-installed-p 'auto-complete)
+	(package-install 'auto-complete))
+
+(if (package-installed-p 'auto-complete)
+	(ac-config-default))
+
+;; Rainbow & Clojure Hook
+
+(if (package-installed-p 'rainbow-delimiters)
+	(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode))
+
+;; Tabs & Python Hook
+
+(add-hook 'python-mode-hook
+	(lambda ()
+		(setq indent-tabs-mode t)
+		(setq python-indent 4)
+		(setq tab-width 4)))
+
+;; Monokai Theme
+
+(if (package-installed-p 'monokai-theme)
+	(load-theme 'monokai t))
